ef38d0674f5a635a8e5428660115d08f90857887
alumno <alumno@vm1>
2020-12-15 23:56:34 +0100

    Terminada la parte de base de datos

--- resources/views/productos/show.blade.php
+++ resources/views/productos/show.blade.php
@@ -6,20 +6,32 @@
 
 	    <div class="col-sm-4">
 
-	        <a href="{{ url('/productos/show/' . $id ) }}">
+	        <a href="{{ url('/productos/show/' . $producto->id ) }}">
 	            <img src="https://picsum.photos/200/300/?random" style="height:200px"/>
 	        </a>
 
 	    </div>
 	    <div class="col-sm-8">
 
-	    	<h4>Nombre: {{$producto[0]}}</h4>
-	    	<h4>Categoria: {{$producto[1]}}</h4>
-	    	<h4>Estado: Producto actualmente comprado</h4>
+	    	<h4>Nombre: {{$producto->nombre}}</h4>
+	    	<h4>Categoria: {{$producto->categoria}}</h4>
+	    	<h4>Estado: 
+	    					@if($producto->pendiente)
+			                    Producto actualmente comprado.
+			                @else
+			                    Producto actualmente no comprado.
+			                @endif
+	    	</h4>
 
-	    	<input type="button" value="Pendiente de compra" style="background-color: red">
-	    	<input type="button" value="Editar Producto" style="background-color: yellow">
-	    	<input type="button" value="Volver al listado" style="background-color: green">
+	    	@if($producto->pendiente)
+                <a class="btn btn-danger" href="#">Comprar</a>
+            @else
+                <a class="btn btn-primary" href="#">Comprado</a>
+            @endif
+            <a class="btn btn-warning" href="{{ url('/productos/edit/' . $producto->id ) }}">
+                <span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>
+                Editar Producto</a>
+            <a class="btn btn-outline-info" href="{{ url('/productos') }}">Volver al listado</a>
 
 	    
 	    </div>
